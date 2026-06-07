64acad68764e38d262f96387e8e7e6e1a7696a66
dt89 <tamalsarkartwoooo@gmail.com>
2026-06-07 14:36:17 +0530

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express' 
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send({message: "Server is Healthy"})
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
