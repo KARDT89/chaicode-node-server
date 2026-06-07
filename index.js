d785a4d691aaf8818684feb760bb465e98ecb7d0
dt89 <tamalsarkartwoooo@gmail.com>
2026-06-07 15:07:06 +0530

    update health check message to indicate version 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8000
 
 app.get('/', (req, res) => {
-  res.send({message: "Server is Healthy"})
+  res.send({message: "Server is Health v3"})
 })
 
 app.listen(port, () => {
